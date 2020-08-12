e83f78d23de3818c901793c0311640244e0ce379
P0159775 <xinhao.yang@pactera.com>
2020-08-12 22:13:47 +0800

    通用的CURD接口

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -47,12 +47,12 @@ export default {
             let res
             if (this.id) {
                 // 存在id说明是编辑
-                res = await this.$http.put(`categories/${this.id}`,this.model)
+                res = await this.$http.put(`rest/categories/${this.id}`,this.model)
             } else {
                 // 新增
                 // 请求接口 提交数据   
                 // 注意判断一下 model 是否有值
-                res = await this.$http.post('categories',this.model)
+                res = await this.$http.post('rest/categories',this.model)
             }
             this.$router.push('/categories/list')
             this.$message({
@@ -61,11 +61,11 @@ export default {
             })
         },
         async fetch(){
-            const res = await this.$http.get(`categories/${this.id}`)
+            const res = await this.$http.get(`rest/categories/${this.id}`)
             this.model = res.data
         },
         async fetchParents(){
-            const res = await this.$http.get(`categories`)
+            const res = await this.$http.get(`rest/categories`)
             this.parents = res.data
         }
     },
--- server/router/admin/index.js
+++ server/router/admin/index.js
@@ -1,43 +1,68 @@
+/**
+ *   req.params.resource：[获取出来的是小写，如： Cannot find module '../../models/categories']
+ *   inflection@1.12.0  :   将 categories 转化成 类名  还可以处理单复数的转换、格式转换等
+ *      const modelName = require('inflection').classify(req.params.resource)
+        const Model = require(`../../models/${modelName}`)
+        将这个做成中间件 放在 app.use('/admin/api/rest/:resource',{ 中间件 },router)
+ */
+
 module.exports = app => {
     const express = require('express');
     // express 的子路由
-    const router = express.Router()
-    const Category = require('../../models/Category')
+    const router = express.Router({ mergeParams:true })  // 把父级的参数合并到url里面来
+    // const  req.Model = require('../../models/ req.Model')
 
     // 创建分类
-    router.post('/categories', async (req,res) => {
-        // 创建数据，通过  Category.create()  , 数据来源： req.body
-       const model = await Category.create(req.body)
+    router.post('/', async (req,res) => {
+        // 创建数据，通过   req.Model.create()  , 数据来源： req.body
+    //    const Model = require(`../../models/${req.params.resource}`)
+       const model = await  req.Model.create(req.body)
        res.send(model)
     })
 
     // 编辑分类 -- 更新
-    router.put('/categories/:id', async (req,res) => {
-        const model = await Category.findByIdAndUpdate(req.params.id,req.body)
+    router.put('/:id', async (req,res) => {
+        
+        const model = await  req.Model.findByIdAndUpdate(req.params.id,req.body)
         res.send(model)
     })
 
     // 编辑分类 -- 删除
-    router.delete('/categories/:id', async (req,res) => {
-        await Category.findByIdAndDelete(req.params.id)
+    router.delete('/:id', async (req,res) => {
+        await  req.Model.findByIdAndDelete(req.params.id)
         res.send({
             success:true
         })
     })
 
     // 分类列表
-    router.get('/categories', async (req,res) => {
+    router.get('/', async (req,res) => {
         // populate() 关联字段查询 好像  加不加 populate() 有什么区别 看看接口返回的数据 加了返回的是一个对象
-       const items = await Category.find().populate('parent').limit(10)
+        // const modelName = require('inflection').classify(req.params.resource)
+        // const Model = require(`../../models/${modelName}`)
+    //    console.log(req.params.resource,modelName)
+    //    const items = await req.Model.find().populate('parent').limit(10)
+    let queryOptions = {}
+    // 模型上有一个属性 它的名称是什么 就是 module.exports = mongoose.model('Category',schema) 其中的额 Category
+    if (req.Model.modelName === 'Category') {
+        queryOptions.populate = 'parent'
+    }
+    const items = await req.Model.find().setOptions(queryOptions).limit(10)
        res.send(items)
     })
 
     // 获取分类详情
-    router.get('/categories/:id', async (req,res) => {
+    router.get('/:id', async (req,res) => {
         // 
-        const model = await Category.findById(req.params.id)
+        const model = await  req.Model.findById(req.params.id)
         res.send(model)
     })
 
-    app.use('/admin/api',router)
+    app.use('/admin/api/rest/:resource',
+        async (req,res,next) => {
+            const modelName = require('inflection').classify(req.params.resource)
+            req.Model = require(`../../models/${modelName}`)
+            next()
+        } 
+    ,router)
 }
