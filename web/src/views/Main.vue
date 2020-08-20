1a40952662a51ee069a45041de8cb645ad44bb31
P0159775 <xinhao.yang@pactera.com>
2020-08-20 22:55:34 +0800

    主页框架和顶部菜单

---
+++ web/src/views/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- top bar -->
+    <div class="topbar bg-black py-2 px-3 d-flex ai-center">
+      <!-- 左边--logo -->
+      <img src="../assets/logo.png" height="30">
+      <!-- 中间 -- 信息 -->
+      <div class="px-2 flex-1">
+        <div class="text-white">王者荣耀</div>
+        <div class="text-grey-1 fs-xxs">团队成就更多</div>
+      </div>
+      <!-- 右边 立即下载 -->
+      <button type="button" class="btn bg-primary">立即下载</button>
+    </div>
+    <!-- 导航栏 -->
+    <div class="bg-primary pt-3 pb-2">
+      <div class="nav d-flex text-white jc-around pb-1">
+        <!-- 1 -->
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">首页</router-link>
+        </div>
+        <!-- 2 -->
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">攻略中心</router-link>
+        </div>
+        <!-- 3 -->
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">赛事中心</router-link>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
