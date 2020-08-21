0a77ab7eae0493f9ad166da632eabef7fcaf44b5
P0159775 <xinhao.yang@pactera.com>
2020-08-21 09:12:50 +0800

    首页轮播- vue swiper

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './style.scss'
+import './assets/scss/style.scss'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// import style
+// import 'swiper/css/swiper.css'
+// import 'swiper/dist/css/swiper.css'
+// If you use Swiper 6.0.0 or higher  
+//  这里的 swiper 版本是 swiper@6.1.2  ，vue-awesome-swiper@4.1.1
+import 'swiper/swiper-bundle.css'
+
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
 
 Vue.config.productionTip = false
 
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    shouye
-  </div>
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg" alt="">
+    </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
+  </swiper>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    name: 'carrousel',
+    data() {
+      return {
+        swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          // Some Swiper option/callback...
+        }
+      }
+    },
+    computed: {
+      swiper() {
+        return this.$refs.mySwiper.$swiper
+      }
+    },
+    mounted() {
+      console.log('Current Swiper instance object', this.swiper)
+      this.swiper.slideTo(3, 1000, false)
+    }
+  }
 </script>
 
-<style>
 
+<style lang="scss">
+@import "../assets/scss/variables";
+
+
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors, "white");
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info");
+    }
+  }
+}
 </style>
